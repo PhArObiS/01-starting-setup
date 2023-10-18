a52ae516fbc013e44988053d19c12dad5da8b0f8
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2023-10-17 20:58:20 -0400

    After chapter 8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,12 +5,13 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item_description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
+
